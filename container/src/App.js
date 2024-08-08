76bb2cefebf46b6c302ea1edb8567701d4c293f3
Mayankkatheriya <mayankkatheriya4@gmail.com>
2024-08-08 16:36:30 +0530

    initial commit

---
+++ container/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { mount } from "marketing/MarketingApp";
+import MarketingApp from "./components/MarketingApp";
+
+console.log(mount);
+
+const App = () => {
+  return <MarketingApp />;
+};
+
+export default App;
